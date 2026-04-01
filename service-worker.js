e203b882aa72727b240aaf328222a365c8285040
Roman Koval <77807168+Roman-Koval@users.noreply.github.com>
2026-04-01 10:56:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,10 @@
 const CACHE = 'travelcar-v1';
-const FILES = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
-];
+const FILES = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
